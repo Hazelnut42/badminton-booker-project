d5f5b67f57849a50bbe86c98f227fa79c0452adc
UCC1222 <you.si@northeastern.edu>
2024-11-18 09:16:17 -0800

    update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 导入 BrowserRouter
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* 用 BrowserRouter 包裹 App */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
