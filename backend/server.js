d9bf501adc49d8f13ea24853429ff9264401d343
ChelseaYuxiChen <chelsea.yuxichen@gmail.com>
2024-11-26 23:22:20 -0800

    Fix static file path and update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ const PORT = process.env.PORT || 5001;
 
 // Serve static files from React build folder
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend/build')));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
     // For all routes, send back the React app
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
     });
 }
 
