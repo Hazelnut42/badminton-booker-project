62437fd5ab85e2f36361880754c58562eafba7a0
ChelseaYuxiChen <chelsea.yuxichen@gmail.com>
2024-12-04 11:43:23 -0800

    update css comments

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,9 @@
+// Configuration object to store the API URL based on the environment
 const config = {
+    // Check if the environment is production or development
     apiUrl: process.env.NODE_ENV === 'production'
-        ? 'https://whispering-caverns-23507-36cfd8145fb6.herokuapp.com/api'
-        : 'http://localhost:5001/api'
+        ? 'https://whispering-caverns-23507-36cfd8145fb6.herokuapp.com/api' // Use the production API URL if in production environment
+        : 'http://localhost:5001/api' // Use the local development API URL if not in production
 };
 
 export default config;
